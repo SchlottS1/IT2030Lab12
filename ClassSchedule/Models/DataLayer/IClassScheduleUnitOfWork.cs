1d22d420c7ad128fe2d72516eb1705715656a648
Spencer <spencerschlott@yahoo.com>
2022-04-19 14:41:17 -0400

    Initial check-in of module Ch12Ex1ClassSchedule

---
+++ ClassSchedule/Models/DataLayer/IClassScheduleUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ClassSchedule.Models
+{
+    public interface IClassScheduleUnitOfWork
+    {
+    public Repository<Day> Days { get; }
+    public Repository<Teacher> Teachers { get; }
+    public Repository<Class> Classes { get; }
+
+    public void Save();
+    }
+}
